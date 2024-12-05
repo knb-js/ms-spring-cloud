9fe1f5ed584b3d8a1de6c075d6ee9ddf0249e6ec
Kahlil Nuñez Bayuk <kah.nunez@duocuc.cl>
2024-12-04 21:23:11 -0300

    initialized module project

---
+++ ms-user-spring-cloud/src/main/java/app/ms/spring/cloud/users/MsUserSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package app.ms.spring.cloud.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUserSpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUserSpringCloudApplication.class, args);
+	}
+
+}
---
+++ ms-user-spring-cloud/src/test/java/app/ms/spring/cloud/users/MsUserSpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package app.ms.spring.cloud.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUserSpringCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
