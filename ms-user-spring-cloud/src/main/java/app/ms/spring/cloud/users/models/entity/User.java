b884f7b6fb1302592a8d35df2918cbe6c319bb37
Kahlil Nuñez Bayuk <kah.nunez@duocuc.cl>
2024-12-06 22:39:37 -0300

    added config properties

---
+++ ms-user-spring-cloud/src/main/java/app/ms/spring/cloud/users/models/entity/User.java
@@ -0,0 +1,22 @@
+package app.ms.spring.cloud.users.models.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
